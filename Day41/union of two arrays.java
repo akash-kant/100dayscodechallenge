087c2496f2bfcba08eccb5c075567bc8a70bf89d
Akash kant <97156436+akash-kant@users.noreply.github.com>
2024-01-22 10:12:04 +0530

    Create union of two arrays.java

---
+++ Day41/union of two arrays.java
@@ -0,0 +1,21 @@
+
+
+//User function Template for Java
+
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        //Your code here
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(a[i]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            set.add(b[i]);
+        }
+
+        return set.size();
+    }
+}
