13fd4dcf8b0a42e1cbbb46370671ea93b70f6477
Akash kant <97156436+akash-kant@users.noreply.github.com>
2024-01-31 15:11:24 +0530

    row with max 1s.java

--- Day50/row with max 1s.java
+++ Day50/row with max 1s.java
@@ -1,1 +1,28 @@
 
+
+//User function Template for Java
+
+class Solution {
+    int rowWithMax1s(int arr[][], int n, int m) {
+        int max = 0;
+        int idx = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int cnt = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] == 1)
+                cnt++;
+            }
+
+            if(cnt > max){
+                max = cnt;
+                idx = i;
+            }
+        }
+        
+        if(max == 0)
+        return -1;
+
+        return idx;
+    }
+}
