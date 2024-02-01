ccb392a48e2559f5f1fbed725c778b4256941d14
Akash kant <97156436+akash-kant@users.noreply.github.com>
2024-02-01 21:50:52 +0530

    Update spirally traversing a matrix.java

--- Day51/spirally traversing a matrix.java
+++ Day51/spirally traversing a matrix.java
@@ -1,30 +1,38 @@
 
 class Solution
 {
-    //Function to check if a string is Pangram or not.
-    public static boolean checkPangram  (String s) {
-        s=s.toLowerCase();
-        HashMap<Character, Integer> hash = new HashMap<>();
-        
-        for(int i = 97; i <= 122; i++)
-        {
-            hash.put((char)i,0);
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    static ArrayList<Integer> spirallyTraverse(int mat[][], int r, int c)
+    {
+        int top = 0;
+        int down = r-1;
+        int left = 0;
+        int right = c-1;
+        int dir = 0;
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        while (top <= down && left <= right) {
+            if(dir==0){// left to right
+                for(int i = left ; i <= right ; i++)
+                   ans.add(mat[top][i]);
+                top++;
+            }
+            else if(dir ==1){// top to down
+                for(int i = top ; i <= down ; i++)
+                  ans.add(mat[i][right]);
+                right--;
+            }
+            else if(dir ==2){//right to left
+                for(int i = right; i >= left ; i--)
+                 ans.add(mat[down][i]);
+                 down--;
+            }
+            else if(dir==3){//down to top
+                for(int i = down; i >= top ; i--)
+                  ans.add(mat[i][left]);
+                left++;
+            }
+            dir = (dir+1)%4;
         }
-        
-        
-       for(int i=0;i<s.length();i++)
-       {char v=s.charAt(i);
-          if(!Character.isWhitespace(v))
-          hash.put(v,hash.getOrDefault(v,0)+1); 
-        }
-        
-      for(Map.Entry<Character, Integer> h:hash.entrySet())  
-      {
-        if(h.getValue()==0)
-          {  //System.out.println(h.getKey());
-            return false;  
-          }
-      }
-     return true;   
+        return ans;
     }
 }
